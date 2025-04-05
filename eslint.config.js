a8e885cf94c3f6b03800222c75646e920a99d9f8
Kaj Kowalski <dev@kajkowalski.nl>
2025-04-05 19:46:21 +0200

    feat: Add ESLint configuration file for code linting

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: "eslint:recommended",
+    rules: {
+        // your custom rules here
+    }
+};
